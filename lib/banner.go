fd9c6e8aad444c0141cad14fedec2578c2f390ca
Yugu_ <1812867714@qq.com>
2022-01-22 17:32:06 +0800

    更新 Banner

--- lib/banner.go
+++ lib/banner.go
@@ -6,14 +6,16 @@ import (
 )
 
 func Banner() {
-	banner := "" +
+	banner := "\n" +
 		"████████╗ █████╗ ██╗ ██████╗ \n" +
 		"╚══██╔══╝██╔══██╗██║██╔═══██╗\n" +
 		"   ██║   ███████║██║██║   ██║\n" +
 		"   ██║   ██╔══██║██║██║   ██║\n" +
 		"   ██║   ██║  ██║██║╚██████╔╝\n" +
 		"   ╚═╝   ╚═╝  ╚═╝╚═╝ ╚═════╝ \n"
-	sub := "Author: binganao\t Github: https://github.com/binganao/Taio"
+	sub := "" +
+		"Author: binganao\t Github: https://github.com/binganao/Taio\n" +
+		"TaiO 的定位是一款用于攻击方对靶标资产梳理，快速定位脆弱资产的工具"
 	fmt.Println(logger.LightWhite(banner))
 	fmt.Println(sub)
 }
