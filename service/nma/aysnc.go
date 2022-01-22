919c7e08fd3ce4b048ff976c9d2b022317978b84
Yugu_ <1812867714@qq.com>
2022-01-22 18:56:22 +0800

    异步 Nmap 扫描，实时出结果

---
+++ service/nma/aysnc.go
@@ -0,0 +1,45 @@
+package nma
+
+import (
+	"context"
+	"github.com/Ullaakut/nmap/v2"
+	"github.com/binganao/Taio/pkg/logger"
+	"strconv"
+	"time"
+)
+
+func aynscNmap(aHost, port string) string {
+
+	var aResult string
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	scanner, err := nmap.NewScanner(
+		nmap.WithTargets(aHost),
+		nmap.WithPorts(port),
+		nmap.WithContext(ctx),
+		nmap.WithServiceInfo(),
+	)
+	if err != nil {
+		logger.Error("Unable to create nma scanner:")
+		return ""
+	}
+
+	result, warnings, err := scanner.Run()
+	if err != nil {
+		logger.Error("Unable to run nma scan")
+		return ""
+	}
+
+	if warnings != nil {
+		logger.Info("Nmap Warnings")
+	}
+
+	host := result.Hosts[0]
+	aPort := host.Ports[0]
+	logger.Info(aHost + " " + strconv.Itoa(int(aPort.ID)) + " " + aPort.Service.Name)
+	aResult = strconv.Itoa(int(aPort.ID)) + ":" + aPort.Service.Name
+
+	return aResult
+}
--- service/nma/nmap.go
+++ service/nma/nmap.go
@@ -1,57 +1,80 @@
 package nma
 
 import (
-	"context"
 	"github.com/binganao/Taio/pkg/logger"
-	"strconv"
-	"time"
-
-	"github.com/Ullaakut/nmap/v2"
+	"runtime"
+	"sync"
 )
 
+//func NmapScan(ip string, portSlice []string) []string {
+//	var ports string
+//	for i, p := range portSlice {
+//		if i == 0 {
+//			ports += p
+//		} else {
+//			ports += "," + p
+//		}
+//	}
+//	var results []string
+//	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+//	defer cancel()
+//
+//	scanner, err := nmap.NewScanner(
+//		nmap.WithTargets(ip),
+//		nmap.WithPorts(ports),
+//		nmap.WithContext(ctx),
+//		nmap.WithServiceInfo(),
+//	)
+//	if err != nil {
+//		logger.Error("Unable to create nma scanner:")
+//		return nil
+//	}
+//
+//	result, warnings, err := scanner.Run()
+//	if err != nil {
+//		logger.Error("Unable to run nma scan")
+//		return nil
+//	}
+//
+//	if warnings != nil {
+//		logger.Info("Nmap Warnings")
+//	}
+//
+//	for _, host := range result.Hosts {
+//		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+//			continue
+//		}
+//
+//		for _, port := range host.Ports {
+//			results = append(results, strconv.Itoa(int(port.ID))+":"+port.Service.Name)
+//		}
+//	}
+//
+//	return results
+//}
+
 func NmapScan(ip string, portSlice []string) []string {
-	var ports string
-	for i, p := range portSlice {
-		if i == 0 {
-			ports += p
-		} else {
-			ports += "," + p
-		}
+	logger.Info("开始对目标 " + ip + " 进行服务识别")
+
+	var wg sync.WaitGroup
+	//var m sync.Mutex
+
+	maxProcs := runtime.NumCPU()
+	runtime.GOMAXPROCS(maxProcs)
+
+	var result []string
+
+	for _, p := range portSlice {
+		wg.Add(1)
+		go func(port string) {
+			r := aynscNmap(ip, port)
+			if r != "" {
+				result = append(result, r)
+			}
+			wg.Done()
+		}(p)
 	}
-	var results []string
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	defer cancel()
+	wg.Wait()
 
-	scanner, err := nmap.NewScanner(
-		nmap.WithTargets(ip),
-		nmap.WithPorts(ports),
-		nmap.WithContext(ctx),
-		nmap.WithServiceInfo(),
-	)
-	if err != nil {
-		logger.Error("Unable to create nma scanner:")
-		return nil
-	}
-
-	result, warnings, err := scanner.Run()
-	if err != nil {
-		logger.Error("Unable to run nma scan")
-		return nil
-	}
-
-	if warnings != nil {
-		logger.Info("Nmap Warnings")
-	}
-
-	for _, host := range result.Hosts {
-		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
-			continue
-		}
-
-		for _, port := range host.Ports {
-			results = append(results, strconv.Itoa(int(port.ID))+":"+port.Service.Name)
-		}
-	}
-
-	return results
+	return result
 }
