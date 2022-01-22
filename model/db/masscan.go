23bcebfdf5db4a77d38d235c68939dc32aa4b9ab
Yugu_ <1812867714@qq.com>
2022-01-22 09:06:33 +0800

    去除部分冗余代码、更新 TODU

--- model/db/masscan.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import "github.com/jinzhu/gorm"
-
-type MascM struct {
-	gorm.Model
-	Host  string
-	Ports string
-}
--- model/db/nmap.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import "github.com/jinzhu/gorm"
-
-type NamM struct {
-	gorm.Model
-	Host    string
-	Service string
-}
