644876c2e6acf3b517a2b4a900ae0d72c5c21919
Yugu_ <1812867714@qq.com>
2022-01-23 19:06:24 +0800

    增加 添加 真实数据功能

--- routes/api/v1/data/add.go
+++ routes/api/v1/data/add.go
@@ -1,5 +1,39 @@
 package data
 
-func AddData() {
+import (
+	"github.com/binganao/Taio/common"
+	"github.com/binganao/Taio/model/response"
+	"github.com/binganao/Taio/service/save"
+	"github.com/binganao/Taio/utils/crypto"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
+func AddData(c *gin.Context) {
+	host, _ := c.GetPostForm("host")
+	ports, _ := c.GetPostForm("ports")
+	services, _ := c.GetPostForm("services")
+	fingers, _ := c.GetPostForm("fingers")
+	secret, _ := c.GetPostForm("secret")
+
+	deSecret := crypto.Base64Decrypto(secret)
+	if deSecret != "" && deSecret != common.SECRET {
+		c.JSON(http.StatusBadRequest, response.OptR{
+			Code: http.StatusBadRequest,
+			Msg:  "操作失败",
+		})
+		return
+	} else {
+		deHost := crypto.Base64Decrypto(host)
+		dePorts := crypto.Base64Decrypto(ports)
+		deServices := crypto.Base64Decrypto(services)
+		deFingers := crypto.Base64Decrypto(fingers)
+		save.Save(deHost, dePorts, deServices, deFingers, time.Now())
+		c.JSON(http.StatusOK, response.OptR{
+			Code: http.StatusOK,
+			Msg:  "操作成功",
+		})
+		return
+	}
 }
