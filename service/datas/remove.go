dde1885007063cb6b440235499bad0af5bd488c2
Yugu_ <1812867714@qq.com>
2022-01-24 22:41:35 +0800

    修复 无法删除临时数据的问题

--- service/datas/remove.go
+++ service/datas/remove.go
@@ -2,10 +2,11 @@ package datas
 
 import (
 	"github.com/binganao/Taio/lib"
+	"github.com/binganao/Taio/model/db"
 	"github.com/binganao/Taio/pkg/logger"
 )
 
 func RemoveTmp(host string) {
-	lib.GetDB().Delete("host = ?", host)
+	lib.GetDB().Where("host = ?", host).Delete(&db.ProbTmpM{})
 	logger.Info("临时数据: " + host + "已删除!")
 }
