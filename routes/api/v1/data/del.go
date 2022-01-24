e6ed101b54c13b84f2faedaee5930902fe3eefeb
Yugu_ <1812867714@qq.com>
2022-01-24 22:51:04 +0800

    完成 删除 真实数据的功能

--- routes/api/v1/data/del.go
+++ routes/api/v1/data/del.go
@@ -1,5 +1,33 @@
 package data
 
-func DelData() {
+import (
+	"github.com/binganao/Taio/common"
+	"github.com/binganao/Taio/model/response"
+	"github.com/binganao/Taio/service/datas"
+	"github.com/binganao/Taio/utils/crypto"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func DelData(ctx *gin.Context) {
+	host, _ := ctx.GetPostForm("host")
+	secret, _ := ctx.GetPostForm("secret")
+
+	deHost := crypto.Base64Decrypto(host)
+	deSecret := crypto.Base64Decrypto(secret)
+
+	if deHost == "" || deSecret == "" || (deSecret != "" && deSecret != common.SECRET) {
+		ctx.JSON(http.StatusBadRequest, response.OptR{
+			Code: http.StatusBadRequest,
+			Msg:  "操作失败",
+		})
+		return
+	} else {
+		datas.Remove(deHost)
+		ctx.JSON(http.StatusOK, response.OptR{
+			Code: http.StatusOK,
+			Msg:  "操作成功",
+		})
+		return
+	}
 }
--- service/datas/remove.go
+++ service/datas/remove.go
@@ -10,3 +10,8 @@ func RemoveTmp(host string) {
 	lib.GetDB().Where("host = ?", host).Delete(&db.ProbTmpM{})
 	logger.Info("临时数据: " + host + "已删除!")
 }
+
+func Remove(host string) {
+	lib.GetDB().Where("host = ?", host).Delete(&db.ProbM{})
+	logger.Warning("数据: " + host + "已删除!")
+}
